a048665232b40fc299a36496c39e70e025c0e76c
lndsilva <lndsilva@hotmail.com>
2023-12-12 11:53:44 -0300

    navegação entre telas

---
+++ src/navegacao/Stack.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import TelaA from '../views/TelaA';
+import TelaB from '../views/TelaB';
+import TelaC from '../views/TelaC';
+import PassoStack from '../components/PassoStack';
+
+const Stack = createNativeStackNavigator();
+
+export default props => (
+  <Stack.Navigator initialRouteName="TelaA" screenOptions={{headerShown: true}}>
+    <Stack.Screen
+      name="TelaA"
+      options={{title: 'Informações Iniciais'}}>
+        {props => (
+          <PassoStack {...props} avancar="TelaB">
+            <TelaA/>
+          </PassoStack>
+        )}
+      </Stack.Screen>
+
+    <Stack.Screen name="TelaB" component={TelaB} />
+    <Stack.Screen name="TelaC" component={TelaC} />
+  </Stack.Navigator>
+);
