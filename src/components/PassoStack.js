a5a6949c6d6e878191ebcf2a0839e7e9c1a89f0b
lndsilva <lndsilva@hotmail.com>
2023-12-14 13:12:58 -0300

    routers com janelas

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -3,19 +3,32 @@ import {Button, View} from 'react-native';
 
 export default props => (
   <View style={{flex: 1}}>
-    <View>
-        {props.avancar
-            ? <Button
-                title="Avançar"
-                onPress={()=>{
-                    props.navigation.navigate(props.avancar)
-                }}
-            />
-            : false
-        }
+    <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+      {props.voltar ? (
+        <Button
+          title="Voltar"
+          onPress={() => {
+            props.navigation.goBack();
+          }}
+        />
+      ) : (
+        false
+      )}
+      {props.avancar ? (
+        <Button
+          title="Avançar"
+          onPress={() => {
+            props.navigation.navigate(
+                props.avancar,
+                props.avancarParams
+                
+            );
+          }}
+        />
+      ) : (
+        false
+      )}
     </View>
-    <View style={{flex: 1}}>
-        {props.children}
-    </View>
+    <View style={{flex: 1}}>{props.children}</View>
   </View>
 );
--- src/navegacao/Stack.js
+++ src/navegacao/Stack.js
@@ -9,17 +9,31 @@ const Stack = createNativeStackNavigator();
 
 export default props => (
   <Stack.Navigator initialRouteName="TelaA" screenOptions={{headerShown: true}}>
-    <Stack.Screen
-      name="TelaA"
-      options={{title: 'Informações Iniciais'}}>
-        {props => (
-          <PassoStack {...props} avancar="TelaB">
-            <TelaA/>
-          </PassoStack>
-        )}
-      </Stack.Screen>
+    <Stack.Screen name="TelaA" options={{title: 'Informações Iniciais'}}>
+      {props => (
+        <PassoStack {...props} avancar="TelaB">
+          <TelaA />
+        </PassoStack>
+      )}
+    </Stack.Screen>
 
-    <Stack.Screen name="TelaB" component={TelaB} />
-    <Stack.Screen name="TelaC" component={TelaC} />
+    <Stack.Screen name="TelaB">
+      {props => (
+        <PassoStack
+          {...props}
+          voltar
+          avancar="TelaC"
+          avancarParams={{numero: 1007}}>
+          <TelaB />
+        </PassoStack>
+      )}
+    </Stack.Screen>
+    <Stack.Screen name="TelaC">
+      {props => (
+        <PassoStack {...props} voltar avancar="TelaC">
+          <TelaC {...props} />
+        </PassoStack>
+      )}
+    </Stack.Screen>
   </Stack.Navigator>
 );
